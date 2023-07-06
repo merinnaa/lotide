063b8f59b49d2428181710c041b96d52a4b0132d
luwam <lwamyohannes2018@gmail.com>
2023-07-06 16:21:48 -0600

    modifies

---
+++ middle.js
@@ -0,0 +1,39 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const  middle = function(array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length <= 2) {
+    return [];
+  } else if (length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+// Test cases
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
