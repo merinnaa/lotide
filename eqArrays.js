5e78b199251ec37da47b796a642b95a48c523462
luwam <lwamyohannes2018@gmail.com>
2023-07-06 15:31:24 -0600

    eqArray modified

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,18 @@
 let eqArrays = function(array1, array2) {
-  // Check if the arrays have the same length
+  
   if (array1.length !== array2.length) {
     return false;
   }
-
-  // Iterate over the elements of the arrays
   for (let i = 0; i < array1.length; i++) {
-    // Check if the elements at the same index are not equal
+    
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
-
-  // All elements are equal
   return true;
 };
-
-// Test cases
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
