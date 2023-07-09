7ca14a851ea11c3a767e760cf6adc8c94b6f05c1
luwam <lwamyohannes2018@gmail.com>
2023-07-08 18:48:53 -0600

    new commit

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+const data = {
+  "blue": { stars: 1 },
+  "green": { stars: 2 },
+  "yellow": { stars: 3 },
+  "red": { stars: 4 }
+};
+
+const result = findKey(data, value => value.stars === 3);
+console.log(result);
+const assertEqual = function(actual, expected) {
+  try {
+    assert.strictEqual(actual, expected);
+    console.log('Assertion passed:', actual, 'equals', expected);
+  } catch (error) {
+    console.error('Assertion failed:', actual, 'does not equal', expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
